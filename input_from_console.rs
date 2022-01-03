690fbace04032a6e120ff52588eef0ef9db5b630
ZEFFERSKIOICH <44055627+ZEFFERSKIOICH@users.noreply.github.com>
2022-01-03 13:06:47 +0530

    Add files via upload
    Taking input from console in rust  demystified

---
+++ input_from_console.rs
@@ -0,0 +1,38 @@
+/*
+ * a)for multiple single variables of same type considering destructing the vector after vin()
+   b)for multiple variables of different types replace vector with tuple and add similar generic types
+     destructing the tuple after  calling vin()
+*/
+
+use std::io;
+//read single value in a line
+#[inline(always)]
+fn cin<T>()->T
+where T: std::str::FromStr, <T as std::str::FromStr>::Err : std::fmt::Debug     //ouput of Err type of FromStr is passed to Debug to unwrap()
+    {
+        let mut val=String::new();
+        io::stdin().read_line(&mut val).unwrap();
+        let val:T=val.trim().parse().unwrap();
+        return val;
+    }
+
+
+//read entire line 
+#[inline(always)]
+fn vin<T>()->Vec<T>
+where T: std::str::FromStr, <T as std::str::FromStr>::Err : std::fmt::Debug     //ouput of Err type of FromStr is passed to Debug to unwrap()
+    {
+        let mut val=String::new();
+        let mut v:Vec<T>=Vec::new();
+        io::stdin().read_line(&mut val).unwrap();
+        for words in val.split_whitespace(){
+            v.push(words.trim().parse::<T>().unwrap());
+        }
+        return v;
+    }
+
+
+fn main(){
+    // let n:usize=cin();
+    // let mut v:Vec<i64>=vin();
+}
